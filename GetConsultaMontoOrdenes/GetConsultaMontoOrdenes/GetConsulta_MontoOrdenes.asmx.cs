18f6d682b1f8ea4c783ede131b6e8de2f47ed70d
Mauricio Garcia <maugarciaflores@gmail.com>
2020-08-22 14:56:25 -0600

    Upda code

--- GetConsultaMontoOrdenes/GetConsultaMontoOrdenes/GetConsulta_MontoOrdenes.asmx.cs
+++ GetConsultaMontoOrdenes/GetConsultaMontoOrdenes/GetConsulta_MontoOrdenes.asmx.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace GetConsultaMontoOrdenes
 {
     /// <summary>
     /// Descripción breve de GetConsulta_MontoOrdenes
     /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
+    [WebService(Namespace = "http://maugarciaapps.tech/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
@@ -21,6 +23,17 @@ namespace GetConsultaMontoOrdenes
         public string HelloWorld()
         {
             return "Hola a todos";
+        }
+
+        [WebMethod]
+        public DataSet GetConsultaMontoOrdenes_()
+        {
+            SqlConnection connnct = new SqlConnection("Server=localhost;Database=AdventureWorks2014; Trusted_connection=True;");
+            connnct.Open();
+            DataSet ds = new DataSet();
+
+            return ds;
+
         }
     }
 }
